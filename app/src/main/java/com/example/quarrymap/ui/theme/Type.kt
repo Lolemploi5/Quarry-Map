c86a6ceb4eba6506c0363186dca7cc56463617dc
Sebastien H. <sebic1708@gmail.com>
2025-04-16 23:47:40 +0200

    Fix GitAction

--- app/src/main/java/com/example/quarrymap/ui/theme/Type.kt
+++ app/src/main/java/com/example/quarrymap/ui/theme/Type.kt
@@ -1,24 +1,23 @@
 package com.example.quarrymap.ui.theme
 
 import android.graphics.Typeface
-import android.content.Context
 
 /**
  * Classe utilitaire pour gérer les polices dans l'application
  */
 object Typography {
     // Obtenir une police normale
-    fun getNormalTypeface(context: Context): Typeface {
+    fun getNormalTypeface(): Typeface {
         return Typeface.create("sans-serif", Typeface.NORMAL)
     }
     
     // Obtenir une police en gras
-    fun getBoldTypeface(context: Context): Typeface {
+    fun getBoldTypeface(): Typeface {
         return Typeface.create("sans-serif", Typeface.BOLD)
     }
     
     // Obtenir une police légère
-    fun getLightTypeface(context: Context): Typeface {
+    fun getLightTypeface(): Typeface {
         return Typeface.create("sans-serif-light", Typeface.NORMAL)
     }
 }
